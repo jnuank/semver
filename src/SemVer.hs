53b917226fdc6911ae50f4f951789924f2ea42eb
Jun Nakajima <ikuta1919@gmail.com>
2019-05-12 10:51:34 +0900

    [mod] お題3:グリーンにした

--- src/SemVer.hs
+++ src/SemVer.hs
@@ -1,7 +1,12 @@
-module SemVer where
+module SemVer(makeVersion) where
 
 data Version = Version { major :: Int,  minor :: Int, patch :: Int } deriving Eq
 
 instance Show Version where
     show (Version major minor patch) = 
         (show major) ++ "." ++ (show minor) ++ "." ++ (show patch)
+
+makeVersion :: Int -> Int -> Int -> Maybe (Version)
+makeVersion ma mi pa 
+    | ma < 0 || mi < 0 || pa < 0 = Nothing
+    | otherwise                  = Just (Version ma mi pa)
--- test/SemVerSpec.hs
+++ test/SemVerSpec.hs
@@ -7,11 +7,20 @@ spec :: Spec
 spec = do
     describe "お題1:文字列表現すること" $ do
         it "1,4,2を渡したら、1.4.2が返ってくる" $ do
-            (show $ Version 1 4 2) `shouldBe` "1.4.2"
+            -- TODO: このJustを消したい
+            (show $ makeVersion 1 4 2) `shouldBe` "Just 1.4.2"
 
     describe "お題2:等価性" $ do 
         it "1.4.2と2.1.0は等しくないこと" $ do 
-            (Version 1 4 2) `shouldNotBe` (Version 2 1 0)
+            (makeVersion 1 4 2) `shouldNotBe` (makeVersion 2 1 0)
         it "1.4.2と1.4.2は等しい" $ do 
-            (Version 1 4 2) `shouldNotBe` (Version 1 4 0)
-
+            (makeVersion 1 4 2) `shouldNotBe` (makeVersion 1 4 0)
+    
+    describe "お題3:負の整数" $ do 
+        it "メジャーに-1を入れたらエラー" $ do 
+            (makeVersion (-1) 4 2) `shouldBe` Nothing
+        it "マイナーに-5を入れたらエラー" $ do 
+            (makeVersion 2 (-5) 4) `shouldBe` Nothing
+        it "パッチに-2を入れたらエラー" $ do 
+            (makeVersion 2 2 (-2)) `shouldBe` Nothing
+    
