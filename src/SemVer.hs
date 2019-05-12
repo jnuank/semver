bdabee80f143a9889886233892e68c90ab69bcb3
Jun Nakajima <ikuta1919@gmail.com>
2019-05-12 10:10:20 +0900

    [add] お題2:等価性をグリーンにした

--- src/SemVer.hs
+++ src/SemVer.hs
@@ -1,6 +1,6 @@
 module SemVer where
 
-data Version = Version { major :: Int,  minor :: Int, patch :: Int } 
+data Version = Version { major :: Int,  minor :: Int, patch :: Int } deriving Eq
 
 instance Show Version where
     show (Version major minor patch) = (show major) ++ "." ++ (show minor) ++ "." ++ (show patch)
