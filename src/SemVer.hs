3b6ef1626a1118ab0c07eea61474da739a4d69bf
Jun Nakajima <ikuta1919@gmail.com>
2019-05-12 10:18:10 +0900

    [mod] リファクタリング

--- src/SemVer.hs
+++ src/SemVer.hs
@@ -3,4 +3,5 @@ module SemVer where
 data Version = Version { major :: Int,  minor :: Int, patch :: Int } deriving Eq
 
 instance Show Version where
-    show (Version major minor patch) = (show major) ++ "." ++ (show minor) ++ "." ++ (show patch)
+    show (Version major minor patch) = 
+        (show major) ++ "." ++ (show minor) ++ "." ++ (show patch)
--- test/SemVerSpec.hs
+++ test/SemVerSpec.hs
@@ -7,15 +7,11 @@ spec :: Spec
 spec = do
     describe "お題1:文字列表現すること" $ do
         it "1,4,2を渡したら、1.4.2が返ってくる" $ do
-            let result = show $ Version 1 4 2
-            result `shouldBe` "1.4.2"
+            (show $ Version 1 4 2) `shouldBe` "1.4.2"
 
     describe "お題2:等価性" $ do 
         it "1.4.2と2.1.0は等しくないこと" $ do 
-            let result = Version 1 4 2
-            let expect = Version 2 1 0
-            result `shouldNotBe` expect
+            (Version 1 4 2) `shouldNotBe` (Version 2 1 0)
         it "1.4.2と1.4.2は等しい" $ do 
-            let result = Version 1 4 2
-            let expect = Version 1 4 2
-            result `shouldBe` expect
+            (Version 1 4 2) `shouldNotBe` (Version 1 4 0)
+
