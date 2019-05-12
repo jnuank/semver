fccab8167aacd81e78649fd01fee83349aa68f72
Jun Nakajima <ikuta1919@gmail.com>
2019-05-13 00:36:57 +0900

    [add] リファクタリング&package.yaml追加

--- src/SemVer.hs
+++ src/SemVer.hs
@@ -1,30 +1,32 @@
-module SemVer (
-    makeVersion,
-    bumpPatch,
-    bumpMinor,
-    bumpMajor
-    ) where
+{-# LANGUAGE QuasiQuotes #-}
 
-import Data.List
+module SemVer
+    ( Version
+    , makeVersion
+    , bumpPatch
+    , bumpMinor
+    , bumpMajor
+    )
+where
+
+import           Data.List
+import           Data.String.Interpolate
 
 data Version = Version { major :: Int,  minor :: Int, patch :: Int } deriving (Eq, Ord)
 
 instance Show Version where
     -- intercalate "." ["1","2","3"]　→　"1.2.3"
-    show (Version major minor patch) = intercalate "." $ show <$> [major , minor, patch]
---                (show major) ++ "." ++ (show minor) ++ "." ++ (show patch)
+    show (Version major minor patch) = [i|#{major}.#{minor}.#{patch}|]
 
+makeVersion :: Int -> Int -> Int -> Maybe Version
+makeVersion major minor patch | major < 0 || minor < 0 || patch < 0 = Nothing
+                              | otherwise = Just (Version major minor patch)
 
-makeVersion :: Int -> Int -> Int -> Maybe (Version)
-makeVersion major minor patch 
-    | major < 0 || minor < 0 || patch < 0   = Nothing
-    | otherwise                             = Just (Version major minor patch)
+bumpPatch :: Version -> Version
+bumpPatch (Version major minor patch) = Version major minor (patch + 1)
 
-bumpPatch :: Version -> Maybe Version
-bumpPatch (Version major minor patch) = Just (Version major minor (patch+1) )
+bumpMinor :: Version -> Version
+bumpMinor (Version major minor _) = Version major (minor + 1) 0
 
-bumpMinor :: Version -> Maybe Version
-bumpMinor (Version major minor _) = Just (Version major (minor+1) 0 )
-
-bumpMajor :: Version -> Maybe Version
-bumpMajor (Version major _ _) = Just (Version (major+1) 0 0 )
+bumpMajor :: Version -> Version
+bumpMajor (Version major _ _) = Version (major + 1) 0 0
--- test/SemVerSpec.hs
+++ test/SemVerSpec.hs
@@ -2,13 +2,14 @@ module SemVerSpec (spec) where
 
 import Test.Hspec
 import SemVer
+import Data.Maybe
+import Data.Functor
 
 spec :: Spec
 spec = do
     describe "お題1:文字列表現すること" $ do
         it "1,4,2を渡したら、1.4.2が返ってくる" $ do
-            -- TODO: このJustを消したい
-            (show $ makeVersion 1 4 2) `shouldBe` "Just 1.4.2"
+            (show $ fromJust $ makeVersion 1 4 2) `shouldBe` "1.4.2"
 
     describe "お題2:等価性" $ do 
         it "1.4.2と2.1.0は等しくないこと" $ do 
@@ -27,15 +28,15 @@ spec = do
     describe "お題4:バージョンアップ" $ do 
         it "下位互換性のあるバグ修正の場合、パッチを更新する" $ do 
             let actual = makeVersion 1 2 1 
-            (actual >>= bumpPatch) `shouldBe` (makeVersion 1 2 2)
+            (actual <&> bumpPatch) `shouldBe` (makeVersion 1 2 2)
 
         it "下位互換性のある機能追加の場合、マイナーを更新して、パッチを0にする" $ do 
             let actual = makeVersion 1 2 1 
-            (actual >>= bumpMinor) `shouldBe` (makeVersion 1 3 0)
+            (bumpMinor <$> actual) `shouldBe` (makeVersion 1 3 0)
 
         it "下位互換性を壊す場合は、メジャーを更新して、マイナーとパッチを0にする" $ do 
             let actual = makeVersion 1 2 1 
-            (actual >>= bumpMajor) `shouldBe` (makeVersion 2 0 0)
+            (bumpMajor <$> actual) `shouldBe` (makeVersion 2 0 0)
 
     describe "お題5:大小比較" $ do 
         it "1.3.9 は 1.4.2 より小さい" $ do 
